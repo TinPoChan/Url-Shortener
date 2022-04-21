814af879cc314d4a640bc9f864ec8dfd7802b150
Tin Po CHAN <ericchantinpo@hotmail.com>
2022-04-21 18:11:34 +0800

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-    const shortUrls = await ShortUrl.find()
+    const shortUrls = await ShortUrl.find({})
     res.render('index', { shortUrls: shortUrls })
   })
 

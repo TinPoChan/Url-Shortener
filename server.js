61ed84d1c73e622f5a73db91fff96d9e33a57c6a
Tin Po CHAN <ericchantinpo@hotmail.com>
2022-04-21 18:07:44 +0800

    Update server.js

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.post('/shortUrls', async (req, res) => {
 app.get('/:shortUrl', async (req, res) => {
     const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
     if (shortUrl == null) { res.send('Short URL not found') }
-    shortUrl.clicks = shortUrl.clicks + 1
-    shortUrl.save()
+    await shortUrl.clicks++
+    await shortUrl.save()
 
     res.redirect(shortUrl.full)
 })
